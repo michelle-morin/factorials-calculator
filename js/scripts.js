ce07f057329fafc071ab972c37177abc88a19feb
mariam alaeddin and Michelle Morin <michelle@>
2020-01-22 16:11:06 -0800

    update logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,20 @@
-// back-end logic: 
+// back-end logic: none.
 
-var total = 1;
-var factorial = function(number) {
-  if (number === 0) {
-    total = total;
-  } else {
-    for (var index = 1; index <= number; index += 1) {
-    total *= index;
-    }
-  }
-}
 
 // front-end logic:
 
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    
+    $("#result").show();
+
     var number = parseInt($("input#number").val());
     
-    factorial(number);
+    var total = 1;
+    for (var index = 1; index <= number; index += 1) {
+      total = total * index;
+    }
 
-    $("#result").empty();
-    $("#result").append(total);    
-    $("#result").show();
+    $("#result").text(total);    
   });
 });
